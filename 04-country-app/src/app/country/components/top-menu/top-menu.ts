70468169397e0de80f179a56eab8c8f503c20b81
Waner-Perez <robertwanerg301@gmail.com>
2026-07-30 22:47:04 -0400

    Country SPA

---
+++ 04-country-app/src/app/country/components/top-menu/top-menu.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+import { Link } from "../../../shared/components/link/link";
+
+@Component({
+  selector: 'app-top-menu',
+  imports: [Link],
+  templateUrl: './top-menu.html',
+})
+export class TopMenu {}
